bed1a8980437af3df3b8a7a9af4dd0f1416d9283
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-08-20 22:45:27 +0800

    Dash navbar updated

--- src/Dashboard/DashNav.jsx
+++ src/Dashboard/DashNav.jsx
@@ -1,14 +1,16 @@
 
-import { HiBars3 } from "react-icons/hi2";
+
 import SearchInput from "../Shared/SearchInput";
 import { FaBars } from "react-icons/fa";
 
 import { IoSearchOutline } from "react-icons/io5";
+import { IoNotifications } from "react-icons/io5";
 
 
 
 
 const DashNav = () => {
+    const notification = true;
     return (
         <>
             <div className="fixed navbar flex w-[86.051%] justify-between items-center h-16 z-10 bg-[#dae1e671] border text-black p-2">
@@ -43,18 +45,58 @@ const DashNav = () => {
                         </ul>
                     </div>
                     <FaBars className="text-[25px] mx-4 cursor-pointer text-[#575A5E]"></FaBars>
-                    <div className="w-full relative mx-5">
+                    <div className="w-3/4 relative mx-5">
                         <SearchInput type="text" className="py-[7px] bg-[#D1D9E2]" placeholder="Search..." /> <IoSearchOutline className="absolute left-3 bottom-[10px] text-[#797E84] cursor-pointer text-lg" ></IoSearchOutline>
                     </div>
                 </div>
-                <div className="navbar-center w-[30%] hidden lg:flex justify-center">
-                    {/* <div className="w-full relative">
-                        <SearchInput type="text" className="py-[7px]" placeholder="Search..." /> <BsSearch className="absolute right-3 bottom-[9px] text-[#000000] cursor-pointer" ></BsSearch>
-                    </div> */}
-                </div>
-                <div className="navbar-end w-[40%]">
+                <div className="navbar-end w-[10%]">
 
                 </div>
+                <div className="navbar-end w-[40%] hidden lg:flex justify-center gap-10">
+                    <div><IoNotifications className="text-[25px] text-[#447BF5]" />
+                        <div className="flex items-center gap-3 relative">{notification ? <span className="bg-[#EB476D] size-4 rounded-full text-[13px] text-white flex justify-center items-center absolute -top-[30px] left-[14px]">6</span> : <></>}</div>
+
+
+
+
+
+
+
+
+
+                    </div>
+                    <div className="flex items-center">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><rect x="1" y="4" width="30" height="24" rx="4" ry="4" fill="#071b65"></rect><path d="M5.101,4h-.101c-1.981,0-3.615,1.444-3.933,3.334L26.899,28h.101c1.981,0,3.615-1.444,3.933-3.334L5.101,4Z" fill="#fff"></path><path d="M22.25,19h-2.5l9.934,7.947c.387-.353,.704-.777,.929-1.257l-8.363-6.691Z" fill="#b92932"></path><path d="M1.387,6.309l8.363,6.691h2.5L2.316,5.053c-.387,.353-.704,.777-.929,1.257Z" fill="#b92932"></path><path d="M5,28h.101L30.933,7.334c-.318-1.891-1.952-3.334-3.933-3.334h-.101L1.067,24.666c.318,1.891,1.952,3.334,3.933,3.334Z" fill="#fff"></path><rect x="13" y="4" width="6" height="24" fill="#fff"></rect><rect x="1" y="13" width="30" height="6" fill="#fff"></rect><rect x="14" y="4" width="4" height="24" fill="#b92932"></rect><rect x="14" y="1" width="4" height="30" transform="translate(32) rotate(90)" fill="#b92932"></rect><path d="M28.222,4.21l-9.222,7.376v1.414h.75l9.943-7.94c-.419-.384-.918-.671-1.471-.85Z" fill="#b92932"></path><path d="M2.328,26.957c.414,.374,.904,.656,1.447,.832l9.225-7.38v-1.408h-.75L2.328,26.957Z" fill="#b92932"></path><path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path></svg>
+                        </div>
+
+                        <form className="max-w-sm mx-auto">
+                            <select id="countries" className="rounded-lg bg-[#E8ECF0]  w-full p-2">
+
+                                <option value="english">English</option>
+                                <option value="bangla">Bangla</option>
+                                <option value="urdu">Urdu</option>
+                                <option value="hindi">Hindi</option>
+                            </select>
+                        </form>
+
+                    </div>
+
+                    <div className=" flex items-center gap-4">
+                        <div className="avatar">
+                            <div className="mask mask-squircle rounded-full h-12 w-12">
+                                <img
+                                    src="https://img.daisyui.com/images/profile/demo/5@94.webp"
+                                    alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className="text-[17px] font-semibold">Mony Roy</h1>
+                            <p className="text-[12px]">Admin</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     );
